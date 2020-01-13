71457babc1c9c7c8f900bd0b60c3c2d1131adc9c
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-13 05:51:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -65,5 +65,5 @@ def checkout(cart, coupons)
   # * apply_clearance
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  consolidate_cart(cart)
+  new_cart = consolidate_cart(cart)
 end
