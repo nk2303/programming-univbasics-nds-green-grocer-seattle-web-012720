dd5549d0708a6623523fe311c8c6214ab86d4484
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-13 05:48:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,7 +53,7 @@ def apply_clearance(cart)
   # REMEMBER: This method **should** update cart
   cart.length.times do |hash|
     if cart[hash][:clearance]
-      cart[hash][:price] = (cart[hash][:price] - cart[hash][:price]*2/10)
+      cart[hash][:price] = (cart[hash][:price] - cart[hash][:price]*2/10)to_f
     end
   end
   return cart
